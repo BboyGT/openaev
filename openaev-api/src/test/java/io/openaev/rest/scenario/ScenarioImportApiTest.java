bf334f129c7c1fbbd890ead58b7378c6088ab3d9
johanah29 <johanah.lekeu@filigran.io>
2026-06-03 23:08:58 +0200

    rollback on mappers
    Signed-off-by: johanah29 <johanah.lekeu@filigran.io>

--- openaev-api/src/test/java/io/openaev/rest/scenario/ScenarioImportApiTest.java
+++ openaev-api/src/test/java/io/openaev/rest/scenario/ScenarioImportApiTest.java
@@ -68,7 +68,7 @@ public class ScenarioImportApiTest extends IntegrationTest {
     injectsImportInput.setName("TEST");
     injectsImportInput.setTimezoneOffset(120);
 
-    when(importMapperRepository.findByIdAndTenantId(any(), any()))
+    when(importMapperRepository.findById(any()))
         .thenReturn(Optional.of(new ImportMapper()));
     when(injectImportService.importInjectIntoScenarioFromXLS(
             any(), any(), any(), any(), anyInt(), anyBoolean()))
@@ -102,7 +102,7 @@ public class ScenarioImportApiTest extends IntegrationTest {
     injectsImportInput.setName("TEST");
     injectsImportInput.setTimezoneOffset(120);
 
-    when(importMapperRepository.findByIdAndTenantId(any(), any()))
+    when(importMapperRepository.findById(any()))
         .thenReturn(Optional.of(new ImportMapper()));
     when(injectImportService.importInjectIntoScenarioFromXLS(
             any(), any(), any(), any(), anyInt(), anyBoolean()))
