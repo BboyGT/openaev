7e4d276cf1f3602b16fd8f5a28cf78fa33a7c176
Gabriel PEZÉ <gabriel.peze@filigran.io>
2026-07-01 09:22:52 +0200

    feat(signature-refactor): spotless (#5151)

--- openaev-api/src/test/java/io/openaev/output_processor/SignatureOutputProcessorTest.java
+++ openaev-api/src/test/java/io/openaev/output_processor/SignatureOutputProcessorTest.java
@@ -1,5 +1,10 @@
 package io.openaev.output_processor;
 
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.*;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.openaev.database.model.ContractOutputTechnicalType;
 import io.openaev.database.model.ContractOutputType;
@@ -12,18 +17,12 @@ import io.openaev.rest.inject.service.ExecutionProcessingContext;
 import io.openaev.rest.inject.service.InjectService;
 import io.openaev.rest.settings.PreviewFeature;
 import io.openaev.service.*;
+import java.util.List;
+import java.util.Optional;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.test.util.ReflectionTestUtils;
-
-import java.util.List;
-import java.util.Optional;
-
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.*;
 
 class SignatureOutputProcessorTest {
 
