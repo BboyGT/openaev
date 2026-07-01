2a0c25676be6075a2485733399619b957357c0e3
Gabriel PEZÉ <gabriel.peze@filigran.io>
2026-07-01 15:57:50 +0200

    feat(signature-refactor): add value to key and fix signature saving (#5151)

--- openaev-api/src/main/java/io/openaev/service/InjectExpectationLockService.java
+++ openaev-api/src/main/java/io/openaev/service/InjectExpectationLockService.java
@@ -29,7 +29,8 @@ public class InjectExpectationLockService {
             .orElseThrow(ElementNotFoundException::new);
 
     if (!expectation.isSignaturesInitialized()) {
-      expectation.setSignatures(signatures);
+      expectation.getSignatures().clear();
+      expectation.getSignatures().addAll(signatures);
     } else {
       expectation.getSignatures().addAll(signatures);
     }
